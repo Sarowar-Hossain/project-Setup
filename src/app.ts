06980fcafaa3345b11232bb5a375127fa0572322
Md Sarowar Hossain <mdsarowar1812@gmail.com>
2023-11-19 13:51:22 +0800

    basic project setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+export const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world');
+});
